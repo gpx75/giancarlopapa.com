a6f0bdcb899f54a21c9655fe847e864f64c9972f
Giancarlo Papa <giancarlo@gpx.dev>
2021-10-22 12:59:29 +0200

    installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <HelloWorld msg="giancarlopapa.com" />
+  <div class="container mx-auto">
+    <HelloWorld msg="giancarlopapa.com" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+// import '../assets/tailwind.css';
+import "@/assets/tailwind.css";
 
 export default {
   name: "App",
@@ -12,14 +16,4 @@ export default {
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
